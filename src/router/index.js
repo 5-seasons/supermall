9c940494b406476704000605ac901cf977e54821
5-seasons <1827956941@qq.con>
2020-11-09 23:53:22 +0800

    10.9

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+const Home = () => import('views/home/Home')
+const Sort = () => import('views/sort/Sort')
+const Cart = () => import('views/cart/Cart')
+const My = () => import('views/my/My')
+
 Vue.use(VueRouter)
 
 const routes = [
-  
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/sort',
+    component: Sort
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/my',
+    component: My
+  }
 ]
 
 const router = new VueRouter({
